b04e960867647f2f4d67900e7bfea04ff7fe17ae
Adharsh R <adharshrajeeev2000@gmail.com>
2023-06-09 22:37:37 +0530

    swagger commented

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,16 +1,16 @@
 import express from 'express'
 import { fetchUser, registerNewUser } from '../controller/userController.js';
 const router=express.Router();
-/**
- * @swagger
- * /:
- *   get:
- *     summary: Get all users
- *     description: Retrieve a list of all users
- *     responses:
- *       200:
- *         description: OK
- */
+// /**
+//  * @swagger
+//  * /:
+//  *   get:
+//  *     summary: Get all users
+//  *     description: Retrieve a list of all users
+//  *     responses:
+//  *       200:
+//  *         description: OK
+//  */
 
 
 router.get('/',fetchUser)
--- server.js
+++ server.js
@@ -3,8 +3,8 @@ import cors from 'cors'
 const app = express()
 import userRoutes from './routes/userRoutes.js'
 import productRoutes from './routes/productRoutes.js'
-import swaggerJSDoc from 'swagger-jsdoc'
-import SwaggerUI from  'swagger-ui-express'
+// import swaggerJSDoc from 'swagger-jsdoc'
+// import SwaggerUI from  'swagger-ui-express'
 import dotenv from 'dotenv'
 import { connectDB } from './database/dbConnection.js'
 
@@ -18,27 +18,27 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors())
 
-const options =  {
-    definition :{
-        openapi:'3.0.0',
-        info:{
-            title:"NODE JS API PROJECT FOR DEVELOPMENT",
-            version:"1.0.0"
-        },
-        servers:[
-        {
-            url:'http://localhost:4000/api/v1'
-        }
-        ]
-    },
-    apis:['./routes/userRoutes.js']
-}
+// const options =  {
+//     definition :{
+//         openapi:'3.0.0',
+//         info:{
+//             title:"NODE JS API PROJECT FOR DEVELOPMENT",
+//             version:"1.0.0"
+//         },
+//         servers:[
+//         {
+//             url:'http://localhost:4000/api/v1'
+//         }
+//         ]
+//     },
+//     apis:['./routes/userRoutes.js']
+// }
 
-const swaggerSpec =  swaggerJSDoc(options)
+// const swaggerSpec =  swaggerJSDoc(options)
 
 
 
-app.use('/api-docs',SwaggerUI.serve,SwaggerUI.setup(swaggerSpec))
+// app.use('/api-docs',SwaggerUI.serve,SwaggerUI.setup(swaggerSpec))
 
 app.use('/api/v1',userRoutes)
 app.use('/api/v1/products',productRoutes)
